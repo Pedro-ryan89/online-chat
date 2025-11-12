08b2546fd2ef8ab5f8a78701b970f71d97ee3616
pedro_ryan89 <pedro_ryan89@archlinux>
2025-11-12 19:41:51 -0300

    Creating the initial project structure

---
+++ src/main/java/dev/pedrory/online/chat/application/ChatApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dev.pedrory.online.chat.application;
+
+/**
+ *
+ * @author pedro_ryan89
+ */
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/dev/pedrory/online/chat/domain/Message.java
@@ -0,0 +1,14 @@
+package dev.pedrory.online.chat.domain;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author pedro_ryan89
+ */
+public class Message {
+    
+}
