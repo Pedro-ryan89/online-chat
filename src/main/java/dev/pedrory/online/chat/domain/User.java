bdd4f5f6064084a4a73d4484a78f66a90da86a2b
pedro_ryan89 <pedro_ryan89@archlinux>
2025-11-18 19:48:47 -0300

    created and testing the basic structure of user class

--- src/main/java/dev/pedrory/online/chat/domain/User.java
+++ src/main/java/dev/pedrory/online/chat/domain/User.java
@@ -4,10 +4,30 @@
  */
 package dev.pedrory.online.chat.domain;
 
+import java.util.Optional;
+import java.util.UUID;
+
 /**
  *
  * @author pedro_ryan89
  */
 public class User {
+    private final String id;
+    private String name;
+    private String currentRoomId;
+    
+    public User(String name,String currentRoomId){
+        this.currentRoomId = currentRoomId;
+        this.name = name;
+        this.id = UUID.randomUUID().toString();
+    }
+    
+    public String getName(String name){return name;}
+    public void setName(String name){this.name = name;}
+    public String getId(){return this.id;}
+    public Optional<String> getCurrentRoomId(){return Optional.ofNullable(currentRoomId);}
+    public void setCurrentRoomId(String roomId){this.currentRoomId = currentRoomId;}
+    
+    
     
 }
