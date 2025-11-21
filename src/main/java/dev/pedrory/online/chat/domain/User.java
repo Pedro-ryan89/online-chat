8b7a46e302889403d1e8f6ae2c250c561175e9f7
pedro_ryan89 <pedro_ryan89@archlinux>
2025-11-21 16:50:22 -0300

    minor change in the User class

--- src/main/java/dev/pedrory/online/chat/domain/User.java
+++ src/main/java/dev/pedrory/online/chat/domain/User.java
@@ -1,33 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dev.pedrory.online.chat.domain;
 
 import java.util.Optional;
 import java.util.UUID;
 
-/**
- *
- * @author pedro_ryan89
- */
 public class User {
+
     private final String id;
     private String name;
     private String currentRoomId;
-    
-    public User(String name,String currentRoomId){
-        this.currentRoomId = currentRoomId;
-        this.name = name;
+
+    public User(String name, String currentRoomId) {
         this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.currentRoomId = currentRoomId;
     }
-    
-    public String getName(String name){return name;}
-    public void setName(String name){this.name = name;}
-    public String getId(){return this.id;}
-    public Optional<String> getCurrentRoomId(){return Optional.ofNullable(currentRoomId);}
-    public void setCurrentRoomId(String roomId){this.currentRoomId = currentRoomId;}
-    
-    
-    
+
+    public String getName() {return this.name;}
+    public void setName(String name) {this.name = name;}
+    public String getId() {return this.id;}
+    public Optional<String> getCurrentRoomId() {return Optional.ofNullable(this.currentRoomId);}
+    public void setCurrentRoomId(String roomId) {this.currentRoomId = roomId;}
 }
